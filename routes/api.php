d9e001fd76b40e879105c1891b1c6383e6d834b0
Suman <sumansarkarwd@gmail.com>
2019-08-13 00:43:28 +0530

    Init

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Jobs;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/jobs', function() {
+    $job = Jobs::all();
+    return response()->json([
+        'data' => $job
+    ]);
+});
